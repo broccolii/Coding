340ec8910830ea79c8bfe6b5b489883bd254b72a
wujingpeng <broccoliii@163.com>
2017-09-21 10:58:37 +0800

    Java Design Patterns

--- Java/DesignPatterns/07_BridgePattern/src/Main.java
+++ Java/DesignPatterns/07_BridgePattern/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Shape redCircle = new Circle(new RedCircle(),100, 100, 10);
+        Shape greenCircle = new Circle(new GreenCircle(), 100, 100, 10);
     }
 }
